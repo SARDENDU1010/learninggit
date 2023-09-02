62cf5dc440b92ffa189d0bbcbe49a2fcd06274bb
Sardendu Kumar <M285796@ONE.merckgroup.com>
2023-09-02 17:56:28 +0530

    added Learning.js also

---
+++ Learning.js
@@ -0,0 +1,14 @@
+const hello ='test';
+const test1 = "test1";
+const test2 = "test2"
+
+
+const test = ()=>{
+  console.log(hello)
+}
+
+const test3 = ()=>{
+  console.log(hello)
+}
+
+alert('hello')
